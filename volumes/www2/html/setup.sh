d0a9e7b142d566e49fdc3e56ba82bab9840a28d8
Axel <axel77g@hotmail.com>
2023-01-09 16:58:13 +0100

    [SETUP SH] check if APP_KEY is not defined in the .env

--- volumes/www2/html/setup.sh
+++ volumes/www2/html/setup.sh
@@ -10,7 +10,13 @@ npm run build
 
 chmod 777 -R ./*
 
-php artisan key:generate
+# Check if APP_KEY is set in .env file 
+var1=$(grep -e "^APP_KEY=" .env)
+var2="APP_KEY="
+
+if [ "$var1" = "$var2" ]; then
+    php artisan key:generate
+fi
 
 php artisan config:cache
 
